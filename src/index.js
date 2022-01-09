471cdf4ce4f3f2692ca9e089984f1cc65b93bcd2
EsraaAlaaOmar <esraa.safa55@gmail.com>
2022-01-09 16:36:26 +0200

    cycle 1

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
+import './styles/App.scss';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import {Provider} from 'react-redux'
+import { createStore,applyMiddleware,compose } from 'redux';
+import thunk from'redux-thunk'
+
+
 import App from './App';
 
-import './styles/App.scss';
+
+
+//import reducers from './reducers/index'
+import reducers from './reduceres/index'
+const composeEnhancers =  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store =createStore(reducers,composeEnhancers(applyMiddleware(thunk)))
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+    <Provider  store={store}>
+        <App />
+    </Provider>
+  ,
   document.getElementById('root')
 );
 
