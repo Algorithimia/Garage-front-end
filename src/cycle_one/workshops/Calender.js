64bcab4b1d5dd831747c0c4e97204169807c454f
EsraaAlaaOmar <esraa.safa55@gmail.com>
2022-01-24 12:13:05 +0200

    note

--- src/cycle_one/workshops/Calender.js
+++ src/cycle_one/workshops/Calender.js
@@ -1,6 +1,8 @@
 import React, { useState,useEffect } from 'react';
 import { Col, Row } from 'react-bootstrap';
 import Calendar from 'react-calendar';
+import ShowMoreText from "react-show-more-text";
+
 import {AiFillPlusCircle} from "react-icons/ai"
 import { FaTools } from 'react-icons/fa';
 import{ MdDeleteSweep} from 'react-icons/md'
@@ -74,7 +76,24 @@ const Calender = () => {
                               <FaTools /> With VEHICLE OWNER
 
                               </div>
-                              <div className='note'><Row> <Col sm={2}><span>Note</span> </Col> <Col sm={10}> <div className='text'>Nulla eleifend pulvinar purus, molestie euismod odio imperdiet ac. Ut sit amet erat nec nibh rhoncus varius in non lorem. Donec interdum, lectus in convallis pulvinar, enim elit porta sapien, vel finibus erat felis sed neque. Etiam aliquet neque sagittis erat tincidunt aliquam. Vestibulum at neque hendrerit, mollis dolor at, blandit justo. Integer ac interdum purus. In placerat lorem non quam pulvinar molestie ac eget lacus. Proin mollis lobortis porttitor. Nam in fringilla felis. Nunc non magna maximus, pulvinar justo ut, pulvinar lacus. Vivamus semper ex vel felis lobortis, nec feugiat erat pulvinar. Cras eu sem sed tellus sodales pellentesque nec id libero. Vestibulum tincidunt, ipsum vitae finibus tempus, orci mi iaculis lacus, id faucibus erat leo ac nisl. Nullam vel pulvinar nisi, ac fringilla urna.</div></Col></Row> </div>
+                              <div className='note'><Row> <Col sm={2}><span>Note</span> </Col> <Col sm={10}> <div className='text'>
+                                
+                                 <ShowMoreText
+                                      /* Default options */
+                                      lines={2}
+                                      more="Show more"
+                                      less="Show less"
+                                      className="content-css"
+                                      anchorClass="my-anchor-css-class"
+                                      // onClick={this.executeOnClick}
+                                      expanded={false}
+                                      width={280}
+                                      truncatedEndingComponent={"... "}
+                                  >
+                                      Nulla eleifend pulvinar purus, molestie euismod odio imperdiet ac. Ut sit amet erat nec nibh rhoncus varius in non lorem. Donec interdum, lectus in convallis pulvinar, enim elit porta sapien, vel finibus erat felis sed neque. Etiam aliquet neque sagittis erat tincidunt aliquam. Vestibulum at neque hendrerit, mollis dolor at, blandit justo. Integer ac interdum purus. In placerat lorem non quam pulvinar molestie ac eget lacus. Proin mollis lobortis porttitor. Nam in fringilla felis. Nunc non magna maximus, pulvinar justo ut, pulvinar lacus. Vivamus semper ex vel felis lobortis, nec feugiat erat pulvinar. Cras eu sem sed tellus sodales pellentesque nec id libero. Vestibulum tincidunt, ipsum vitae finibus tempus, orci mi iaculis lacus, id faucibus erat leo ac nisl. Nullam vel pulvinar nisi, ac fringilla urna.
+                                  </ShowMoreText>
+                                
+                                </div></Col></Row> </div>
                              
                               
 
--- src/cycle_one/workshops/components/Car.js
+++ src/cycle_one/workshops/components/Car.js
@@ -4,6 +4,7 @@ import { Link, Route,Routes } from 'react-router-dom'
 import { Col, Row } from "react-bootstrap"
 import {RiEditLine}from "react-icons/ri"
 import {MdDeleteSweep} from "react-icons/md"
+import ShowMoreText from 'react-show-more-text'
 
 const Car = ({view= false}) => {
     const [show,setShow]=useState(view)
@@ -20,7 +21,22 @@ const Car = ({view= false}) => {
                    <div className='line'></div>
              
               </div>
-              <div className='note'><Row> <Col sm={3}><span>Note</span> </Col> <Col sm={9}> <div className='text'>Nulla eleifend pulvinar purus, molestie euismod odio imperdiet ac. Ut sit amet erat nec nibh rhoncus varius in non lorem. Donec interdum, lectus in convallis pulvinar, enim elit porta sapien, vel finibus erat felis sed neque. Etiam aliquet neque sagittis erat tincidunt aliquam. Vestibulum at neque hendrerit, mollis dolor at, blandit justo. Integer ac interdum purus. In placerat lorem non quam pulvinar molestie ac eget lacus. Proin mollis lobortis porttitor. Nam in fringilla felis. Nunc non magna maximus, pulvinar justo ut, pulvinar lacus. Vivamus semper ex vel felis lobortis, nec feugiat erat pulvinar. Cras eu sem sed tellus sodales pellentesque nec id libero. Vestibulum tincidunt, ipsum vitae finibus tempus, orci mi iaculis lacus, id faucibus erat leo ac nisl. Nullam vel pulvinar nisi, ac fringilla urna.</div></Col></Row> </div>
+              <div className='note'><Row> <Col sm={3}><span>Note</span> </Col> <Col sm={9}> <div className='text'>
+                                <ShowMoreText
+                                      /* Default options */
+                                      lines={2}
+                                      more="Show more"
+                                      less="Show less"
+                                      className="content-css"
+                                      anchorClass="my-anchor-css-class"
+                                      // onClick={this.executeOnClick}
+                                      expanded={false}
+                                      width={280}
+                                      truncatedEndingComponent={"... "}
+                                  >
+                                      Nulla eleifend pulvinar purus, molestie euismod odio imperdiet ac. Ut sit amet erat nec nibh rhoncus varius in non lorem. Donec interdum, lectus in convallis pulvinar, enim elit porta sapien, vel finibus erat felis sed neque. Etiam aliquet neque sagittis erat tincidunt aliquam. Vestibulum at neque hendrerit, mollis dolor at, blandit justo. Integer ac interdum purus. In placerat lorem non quam pulvinar molestie ac eget lacus. Proin mollis lobortis porttitor. Nam in fringilla felis. Nunc non magna maximus, pulvinar justo ut, pulvinar lacus. Vivamus semper ex vel felis lobortis, nec feugiat erat pulvinar. Cras eu sem sed tellus sodales pellentesque nec id libero. Vestibulum tincidunt, ipsum vitae finibus tempus, orci mi iaculis lacus, id faucibus erat leo ac nisl. Nullam vel pulvinar nisi, ac fringilla urna.
+                               </ShowMoreText>
+                  </div></Col></Row> </div>
        </div>}
       </div>
     )
