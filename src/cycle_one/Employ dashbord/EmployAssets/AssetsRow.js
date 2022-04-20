928d660d3041be3ff93144d66b90cd5701dd6712
EsraaAlaaOmar <esraa.safa55@gmail.com>
2022-04-21 01:39:30 +0200

    Employ Assettsassets

--- src/cycle_one/Employ dashbord/EmployAssets/AssetsRow.js
+++ src/cycle_one/Employ dashbord/EmployAssets/AssetsRow.js
@@ -1,32 +1,57 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import moment from 'moment'
 import OverlayList from '../../workshops/OverlayList'
-const AssetsRow = ({type ,num, name,assignment ,date }) => {
-    return (
-        <tr>
-           
-            <th className={type}>{type}</th>
-            <th> {num}</th>
-            <th>{name}</th>
-            <th>{date} </th>
-            <th> </th>
-            <th> </th>
-            <th> </th>
-            <th> </th>
-            <th> </th>
-            <th><th className='moreIcon'>
+const AssetsRow = ({asset}) => {
+        return (
+            <tr>
+               
+                <th className={asset.type && asset.type.title}>{asset.type && asset.type.title}</th>
+                <th> {asset.id}</th>
+                <th>{asset.title}</th>
+                <th>{moment(asset.created_at).format("DD-MM-YYYY")}</th>  
+                <th> </th>
+                <th> </th>
+                <th> </th>
+                <th> </th>
+                <th> </th>
+                <th className='moreIcon'>
         
 
-        <OverlayList option1='ASSIGN TO WORKORDER' option1_path='/employ/assets'
+             <OverlayList option1='ASSIGN TO WORKORDER' option1_path='/employ/assets'
+                     
+                       
+                          />
+           
+              </th>
+            </tr>    
+               
+        )
+    // return (
+    //     <tr>
+           
+    //         <th className={type}>{type}</th>
+    //         <th> {num}</th>
+    //         <th>{name}</th>
+    //         <th>{date} </th>
+    //         <th> </th>
+    //         <th> </th>
+    //         <th> </th>
+    //         <th> </th>
+    //         <th> </th>
+    //         <th><th className='moreIcon'>
+        
+
+    //     <OverlayList option1='ASSIGN TO WORKORDER' option1_path='/employ/assets'
                  
                    
-                     />
+    //                  />
        
-         </th>
-     </th>
-        </tr>    
+    //      </th>
+    //  </th>
+    //     </tr>    
            
-    )
+    // )
 }
 
 export default AssetsRow
--- src/cycle_one/Employ dashbord/EmployAssets/EmployAssets.js
+++ src/cycle_one/Employ dashbord/EmployAssets/EmployAssets.js
@@ -1,15 +1,20 @@
-import React , {useState} from 'react'
+import React , {useEffect, useState} from 'react'
 import {Link, Route, Routes} from 'react-router-dom'
 import {GoTriangleDown,GoTriangleUp} from "react-icons/go"
 import {AiFillPlusCircle} from "react-icons/ai"
 import {FaSearch} from 'react-icons/fa'
-
+import { useDispatch,useSelector } from 'react-redux'
+import {getAssets, editeAsset,clearstate,getAssettypes} from '../../../store/store slices/assetSlice'
 import AssetsRow from './AssetsRow'
 
 const EmployAssets = () => {
-
+    const dispatch = useDispatch()
+    const {assets, isLoading,created, error} = useSelector((state)=>state.assets)
+    const [showAlert, setShowAlert]= useState(true)
     const [entries, setEntries] = useState(0);
     const [date, setDate] =  useState(0);
+    const [search , setSearch] = useState('')
+   
     const upEntries=()=>{
        setEntries(parseInt(entries)+1)
     }
@@ -25,82 +30,113 @@ const EmployAssets = () => {
         setDate(parseInt(date)-1)
       
      }
+     useEffect(() =>{
+        dispatch(getAssets());
+        const timeId = setTimeout(() => {
+            // After 3 seconds set the showAlert value to false
+            setShowAlert(false)
+             dispatch(clearstate())
+          }, 5000)
+      
+          return () => {
+            clearTimeout(timeId)
+          }
+     
+       
+    
+      },[dispatch])
+      const onChange=e=>{setSearch(e.target.value.toLowerCase())}
+     
+     
+
+      //search 
+      const searchResult = assets.filter((el) => {
+   
+        if (search === '') {
+            return el
+        }
+      
+        else {        
+            return ( el.title.toLowerCase().includes(search) )           
+        }
+    })
+      const renderedAssets = searchResult.map(asset =>{
+          return    <AssetsRow asset={asset}  key={asset.id}/>
+      })
+    console.log(searchResult)
+      
     return (
-        <div className='assets'>
-             <div className='header'>
-                 ASSIGNED ASSETS
-              
+        <>
+            {isLoading ? <img className='loading-img' src="/images/giphy.gif" /> :
+            <div className='assets'>
+                <div className='header'>
+                    ASSIGNED ASSETS
+                
 
-             </div>
-             <div className='second_row'>
-         <div className='inline-block input_block'>
-                       <span>
-                       Show Entries
-                       </span>
-                       <input type='number'  onChange={(e)=> setEntries(e.target.value)} value={entries}/>
-                       <div className="change_number">
-                        <div className='change' onClick={upEntries}>   
-                         <GoTriangleUp />
-                        </div>
-                        <div className="up change" onClick={downEntries}>
-                         <GoTriangleDown />
-                        </div>
-                       </div>
-                       
-                    </div>
-                    
-                    <div className='inline-block input_block'>
+                </div>
+                <div className='second_row'>
+            <div className='inline-block input_block'>
                         <span>
-                        Date
+                        Show Entries
                         </span>
-                        <input type='number'  value={date} onChange={(e)=> setDate(e.target.value)}/>
+                        <input type='number'  onChange={(e)=> setEntries(e.target.value)} value={entries}/>
                         <div className="change_number">
-                            <div className='change' onClick={upDate}>
-                             <GoTriangleUp />
+                            <div className='change' onClick={upEntries}>   
+                            <GoTriangleUp />
                             </div>
-                            <div className="up change" onClick={downDate}>
-                             <GoTriangleDown />
+                            <div className="up change" onClick={downEntries}>
+                            <GoTriangleDown />
                             </div>
                         </div>
+                        
                         </div>
-                       
-                     <div className='search'>
-                         <input placeholder='Search Assets' />
-                         <span> <FaSearch /> </span>
+                        
+                        <div className='inline-block input_block'>
+                            <span>
+                            Date
+                            </span>
+                            <input type='number'  value={date} onChange={(e)=> setDate(e.target.value)}/>
+                            <div className="change_number">
+                                <div className='change' onClick={upDate}>
+                                <GoTriangleUp />
+                                </div>
+                                <div className="up change" onClick={downDate}>
+                                <GoTriangleDown />
+                                </div>
+                            </div>
+                            </div>
+                        
+                        <div className='search'>
+                            <input placeholder='Search Assets'  onChange={(e)=>onChange(e)}/>
+                            <span> <FaSearch /> </span>
 
-                     </div>
+                        </div>
+
+                            
+            </div>
+            <table>
+                    <thead>
+                        <tr>
+                            <th> ASSET  <br/> TYPE </th>
+                            <th> ASSET <br/> NUMBER</th>
+                            <th> ASSET <br/> NAME</th>
+                            <th>DATE </th>
+                            <th> </th>
+                            <th> </th>
+                            <th> </th>
+                            <th> </th>
+                            <th> </th>
+                            <th>  </th>
+                        </tr>
 
                         
-         </div>
-         <table>
-                <thead>
-                    <tr>
-                        <th> ASSET  <br/> TYPE </th>
-                        <th> ASSET <br/> NUMBER</th>
-                        <th> ASSET <br/> NAME</th>
-                        <th>DATE </th>
-                        <th> </th>
-                        <th> </th>
-                        <th> </th>
-                        <th> </th>
-                        <th> </th>
-                        <th>  </th>
-                    </tr>
-
+                    </thead>
+                   {renderedAssets}
                     
-                </thead>
-                <AssetsRow type='SPARE PART' num='1342' name='Eget nisi'assignment='EMPLOYEE NAME' date='12/6'/>
-                <AssetsRow type='ASSET TYPE' num='1342' name='Eget nisi'assignment='EMPLOYEE NAME' date='12/6'/>
-                <AssetsRow type='SPARE PART' num='1342' name='Eget nisi'assignment='EMPLOYEE NAME' date='12/6'/>
-                <AssetsRow type='ASSET TYPE' num='1342' name='Eget nisi'assignment='EMPLOYEE NAME' date='12/6'/>
-                <AssetsRow type='SPARE PART' num='1342' name='Eget nisi'assignment='EMPLOYEE NAME' date='12/6'/>
-                <AssetsRow type='ASSET TYPE' num='1342' name='Eget nisi'assignment='EMPLOYEE NAME' date='12/6'/>
-                <AssetsRow type='SPARE PART' num='1342' name='Eget nisi'assignment='EMPLOYEE NAME' date='12/6'/>
-                <AssetsRow type='ASSET TYPE' num='1342' name='Eget nisi'assignment='EMPLOYEE NAME' date='12/6'/>
-                
-          </table>     
-        
-        </div>
+            </table>     
+            
+            </div>}
+        </>
     )
 }
 
